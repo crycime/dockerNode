1f523a47464d6f50e675e8ebf803a11cc425e051
lizizhen <lizizhen@cisystemsolutions.com>
2019-02-27 14:53:26 +0800

    cesd

--- test/user.test.ts
+++ test/user.test.ts
@@ -2,6 +2,7 @@ import { UserDAO } from '../src/components/user/dao/index';
 import 'mocha';
 describe('User DAO层测试', function() {
     it('用例1', async function() {
+        //
         const dao = new UserDAO();
         const y = await dao.findAll();
         return true;
