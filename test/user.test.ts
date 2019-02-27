727773c5415f79ce2ff8af2f62c88a4256988485
lizizhen <lizizhen@cisystemsolutions.com>
2019-02-27 14:56:31 +0800

    mocha自动化测试

--- test/user.test.ts
+++ test/user.test.ts
@@ -2,7 +2,6 @@ import { UserDAO } from '../src/components/user/dao/index';
 import 'mocha';
 describe('User DAO层测试', function() {
     it('用例1', async function() {
-        //
         const dao = new UserDAO();
         const y = await dao.findAll();
         return true;
